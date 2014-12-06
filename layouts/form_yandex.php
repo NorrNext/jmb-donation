fffdf1b5c36d4b9ed44043f8fb54ddf0f6342650
shtier <valchuk.artem@gmail.com>
2014-12-06 13:25:12 +1000

    fix input style

--- layouts/form_yandex.php
+++ layouts/form_yandex.php
@@ -9,7 +9,7 @@
 defined('_JEXEC') or die;
 ?>
 
-<form class="jmb-donation-form-yandex form-inline" action="https://money.yandex.ru/charity.xml" method="post">
+<form class="jmb-donation-form-yandex uk-form form-inline" action="https://money.yandex.ru/charity.xml" method="post">
 	<?php if ($displayData->params->get('show_effects', 1)) : ?>
 		<?php echo JLayoutHelper::render('effects', $displayData); ?>
 	<?php endif; ?>
